7a26b5b462466b71ca65d45db97d244856099b23
cmorela80525 <c.morelandj@gmail.com>
2022-10-10 10:18:28 -0400

    answer reveal functioning

--- src/components/GetQuestion.js
+++ src/components/GetQuestion.js
@@ -10,24 +10,52 @@ export default function GetQuestion(props) {
       .then((resp) => resp.json())
       .then((triviaData) => setTriviaData(triviaData[0]))
       .catch((error) => console.log(error))
-  },[])
+      
+  }, [])
+
+
+  const {value,question,answer} = triviaData
+      //console.log(triviaData.category.title)
+    //const {value,question,answer} = triviaData
+    //const {category:{title}} = triviaData
+    
   
-    //const category = triviaData.category.title
-    const {value,question,answer} = triviaData
    
-function handleClick() {
+    const [isAnswerClicked, setAnswerClicked] =useState(false)
+
+function handleQuestionClick() {
   window.location.reload()
 }
 
+function handleAnswerClick() {
+  setAnswerClicked(true)
+}
+
+if(isAnswerClicked === false) {
   return (
     <>
       <h2>Let's Play!</h2>
-      <button onClick={handleClick}>Get random trivia question</button>
+      <button onClick={handleQuestionClick}>Get random trivia question</button>
       <h3>Cetegory: {}</h3>
       <h4>Points: {value}</h4>
       <h4>Question: {question}</h4>
-      <h4>Answer: {answer}</h4>
-      <button onClick={()=>{}}>Click to Reveal Answer</button>
+      <button onClick={handleAnswerClick}>Click to Reveal Answer</button>
     </> 
   )
+
+}
+
+if(isAnswerClicked ) {
+ return (
+    <>
+      <h2>Let's Play!</h2>
+      <button onClick={handleQuestionClick}>Get random trivia question</button>
+      <h3>Cetegory: {}</h3>
+      <h4>Points: {value}</h4>
+    
+      <h4>Answer: {answer}</h4>
+      <button onClick={handleAnswerClick}>Click to Reveal Answer</button>
+    </> 
+  )
+}
 }
